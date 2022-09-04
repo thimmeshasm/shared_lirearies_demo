697e4d9539edbcff258d9319a8673fb6f752ccfe
Siddeshg672 <54892886+Siddeshg672@users.noreply.github.com>
2022-09-04 09:30:11 +0530

    Update gitLogin.groovy

--- vars/gitLogin.groovy
+++ vars/gitLogin.groovy
@@ -1,1 +1,5 @@
-#
+def gitStatus(branch){
+  git branch: "${branch}", credentialsId: 'gitCred', url: 'https://github.com/Siddeshg672/hello_world_public_war.git'
+  sh "mvn clean install"
+  sh "cp -R webapp/target/webapp.war ."
+}
