272414ef613cc6b51dbe1cc83d2c22a3fb41e6ff
Siddeshg672 <54892886+Siddeshg672@users.noreply.github.com>
2023-10-10 09:04:18 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -2,6 +2,6 @@ def docker_push(user,pass,image,repo){
   sh "docker login -u ${user} -p ${pass}"
   sh "docker pull ${image}"
   sh "docker tag ${image} ${repo}/${image}"
-  sh "docker login -u ${user} -p ${pass} ${repo}"
+  sh "docker login -u ${user} -p ${pass}"
   sh "docker push ${repo}/${image}"
 }
