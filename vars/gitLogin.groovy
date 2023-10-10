c314c49480f9b87f3011088f8bad5ce007a22269
THIMMESHA S M <136349958+thimmeshasm@users.noreply.github.com>
2023-10-10 15:35:52 +0530

    Update gitLogin.groovy

--- vars/gitLogin.groovy
+++ vars/gitLogin.groovy
@@ -1,5 +1,5 @@
 def gitStatus(branch){
-  git branch: "${branch}", credentialsId: 'gitCred', url: 'https://github.com/Siddeshg672/hello_world_public_war.git'
+  git branch: "${branch}", credentialsId: 'gitCred', url: 'https://github.com/thimmeshasm/hello_world_public_war.git'
   sh "mvn clean install"
   sh "cp -R webapp/target/webapp.war ."
 }
